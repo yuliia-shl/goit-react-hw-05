b9cce319a2212494417777994bb078b0db9b45af
Yuli <yu.expert@gmail.com>
2024-12-08 21:07:23 +0200

    Add styles MovieCast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,4 +1,4 @@
-// import s from "./MovieCast.module.css"
+import s from "./MovieCast.module.css"
 
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
@@ -22,13 +22,14 @@ export const MovieCast = () => {
 
   return (
     <div>
-      <ul>
+      <ul className={s.castList}>
         {credits.map((credit) => (
-          <li key={credit.id}>
+          <li key={credit.id} className={s.castItem}>
             <img
               src={`https://image.tmdb.org/t/p/w500/${credit.profile_path}`}
               alt={`${credit.name}'s photo`}
-              width={140}
+              width={240}
+              height={360}
             />
             <h4>{credit.name}</h4>
             <p>Character: {credit.character}</p>
